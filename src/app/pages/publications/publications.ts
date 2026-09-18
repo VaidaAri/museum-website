3cbc7993397d1c07a9ea1f8f49d19b2ed6e733ba
ariabcd <ariana.vaida@stud.ubbcluj.ro>
2026-09-18 15:14:32 +0300

    Add main page components

---
+++ src/app/pages/publications/publications.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-publications',
+  styleUrl: './publications.scss',
+  templateUrl: './publications.html',
+})
+export class Publications {}
