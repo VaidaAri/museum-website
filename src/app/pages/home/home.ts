fad2b26ac1274b62c89de1e4b0509bff2e998038
ariabcd <ariana.vaida@stud.ubbcluj.ro>
2026-09-24 11:26:38 +0300

    Add home page layout

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  imports: [],
   selector: 'app-home',
-  styleUrl: './home.scss',
+  imports: [],
   templateUrl: './home.html',
+  styleUrl: './home.scss',
 })
 export class Home {}
